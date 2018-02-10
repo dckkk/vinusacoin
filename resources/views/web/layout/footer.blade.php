0b32ef6a6adde609d5bcadb431b2742f80b5a496
ridhozhr10 <azharridho42@gmail.com>
2018-02-11 00:07:17 +0700

    update footer

--- resources/views/web/layout/footer.blade.php
+++ resources/views/web/layout/footer.blade.php
@@ -1,24 +1,27 @@
 <footer>
 	<div class="container">
-		<div class="col-sm-12 col-md-3">
+		<div class="col-sm-12 col-md-3 footer-menu">
 			<ul class="list-group">
-				<li class="list-group-item">First item</li>
-				<li class="list-group-item">Second item</li>
-				<li class="list-group-item">Third item</li>
+				<li class="bottom-link"><a href="#">First item</a></li>
+				<li class="bottom-link"><a href="#">Second item</a></li>
+				<li class="bottom-link"><a href="#">Third item</a></li>
+				<li class="bottom-link"><a href="#">Forth item</a></li>
 			</ul>
 		</div>
-		<div class="col-sm-12 col-md-3">
+		<div class="col-sm-12 col-md-3 footer-menu">
 			<ul class="list-group">
-				<li class="list-group-item">First item</li>
-				<li class="list-group-item">Second item</li>
-				<li class="list-group-item">Third item</li>
+				<li class="bottom-link"><a href="#">First item</a></li>
+				<li class="bottom-link"><a href="#">Second item</a></li>
+				<li class="bottom-link"><a href="#">Third item</a></li>
+				<li class="bottom-link"><a href="#">Forth item</a></li>
 			</ul>
 		</div>
-		<div class="col-sm-12 col-md-3">
+		<div class="col-sm-12 col-md-3 footer-menu">
 			<ul class="list-group">
-				<li class="list-group-item">First item</li>
-				<li class="list-group-item">Second item</li>
-				<li class="list-group-item">Third item</li>
+				<li class="bottom-link"><a href="#">First item</a></li>
+				<li class="bottom-link"><a href="#">Second item</a></li>
+				<li class="bottom-link"><a href="#">Third item</a></li>
+				<li class="bottom-link"><a href="#">Forth item</a></li>
 			</ul>
 		</div>
 		<div class="col-sm-12 col-md-3">
@@ -29,19 +32,4 @@
 				<a href="#"><img src="/img/icon/youtube.svg" alt=""></a>
 			</div>
 		</div>
-		{{-- <div class="row bottom-logo">
-			<!-- logo footer begin -->
-			<div id="footer-logo">
-				<a href="/"><img src="/ff/images/gain_dark_bottom.png" alt="GainMax Capital Limited" style="width:160px;"></a>
-				<p><span>Copyright ©2017 GainMax Capital Limited. All Rights Reserved.</span></p>
-			</div>
-			<!-- logo footer end -->
-			<hr>
-			<div class="uk-margin-small-top">
-				<p>GainMax Capital Limited is an Financial Company registered in UNITED KINGDOM, number 10829908. Any reference to the advisory services refers to GainMax Capital Limited<br>
-				Any copying, reproduction, republication, as well as on the Internet resources of any materials from www.gainmax.co.uk is possible only upon written permission.</p>
-			</div>
-		</div> --}}
-	</div>
-	{{$page_title}}
 </footer>
