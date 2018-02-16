ad4fde60f9d6c4d56e63bb8737df1bf6ea539a20
ridhozhr10 <azharridho42@gmail.com>
2018-02-16 21:37:06 +0700

    fix indent using tabs

--- resources/views/admin/layout/css.blade.php
+++
@@ -1,8 +0,0 @@
-{{--
-    isinya core admin LTE
-        -> bootstrap
-        -> adminLTE.css
-        -> fontawesome
-        -> ionicons
---}}
-<link rel="stylesheet" href="{{ asset('css/AdminLTE.css') }}">
--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -1,36 +1,37 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>AdminLTE 2 | @yield('page_title')</title>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    {{-- main style --}}
-    <link rel="stylesheet" href="{{ asset('css/AdminLTE.css') }}">
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+		<title>AdminLTE 2 | @yield('page_title')</title>
 
-    @yield('css')
-  </head>
-  <body class="hold-transition skin-blue sidebar-mini">
-    <div class="wrapper">
-      
-      @component('admin/layout/header', $header)
-      @endcomponent
+		{{-- main style --}}
+    {{-- adminLTE including --}}
+    {{-- bootstrap --}}
+    {{-- main css adminLTE --}}
+    {{-- fontawesome --}}
+    {{-- ionicons --}}
+		<link rel="stylesheet" href="{{ asset('css/AdminLTE.css') }}">
 
-      <!-- Content Wrapper. Contains page content -->
+		@yield('css')
+	</head>
+	<body class="hold-transition skin-blue sidebar-mini">
+		<div class="wrapper">
+			@component('admin/layout/header', $header)
+			@endcomponent
+			
       <div class="content-wrapper">
-        @yield('content')
-      </div>
-      <!-- /.content-wrapper -->
+				@yield('content')
+			</div>
 
-      @component('admin/layout/footer')
-      @endcomponent
-    </div>
-    @component('admin/layout/javascript')
-    @endcomponent
+			@component('admin/layout/footer')
+			@endcomponent
+		</div>
+		@component('admin/layout/javascript')
+		@endcomponent
 
-    @yield('javascript')
-  </body>
+		@yield('javascript')
+	</body>
 </html>
