6ffdac2fa6dadd85b23bfb2e2bf6260e34311679
ridhozhr10 <azharridho42@gmail.com>
2018-03-09 02:45:20 +0700

    translate content about us

---
+++ resources/lang/id/about_us.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	"Vinusacoin adalah cryptocurrency, visi dan misi Vinusacoin adalah menyatukan dunia dengan transaksi cryptocurrency, dimana nantinya keuntungan yang didapat dapat anda percayai.",
+	"kenapa anda harus  mempercayai Vinusacoin, karena Vinusacoin mempunya nilai tersendiri dibanding dengan cryptocurrency, yaitu hanya dengan menghitung nilai saat ICO, sejak awal Vinusacoin sudah memiliki poin penjualan yang baik dan pastinya dapat anda perhitungkan.",
+	"Kita harus mengerti bahwa transaksi dalam cryptocurrency mempunyai resiko kegagalan yang besar, tetapi juga memiliki peluang keuntungan yang memuaskan.",
+	"Dengan melihat faktor ini, kami mulai membuat sistem transaksi cryptocurrency, karena cryptocurrency modern dapat mendatangkan keuntungan dengan resiko yang lebih kecil. Seperti yang kita tahu dalam dunia trading kekuatan adalah kuncinya, semakin banyak kebutuhannya maka keuntungan yang di dapat pastinya maksimal dan konsisten, Vinusacoin akan memberikan keuntungan yang konsisten.",
+	"Vinusacoin mempunyai platform mumpuni dengan kemanan yang terjamin. Platform ini mulai menyebar ke Asia Tenggara dan Eropa, yang dengan itu semua orang sekarang dapat mengakses Kredit Digital, Investasi Digital dan Layanan Transaksi.",
+	"Dengan bergabung pada Vinusacoin, bukan hanya member yang dapat mengakses Kredit Digital, Investasi Digital, atau Layanan Belanja; nantinya perjalanan dan pembangunan Vinusacoin akan memudahkan transasi online pada dunia fintech.",
+	"VINUSACOIN MEMUDAHKAN ANDA UNTUK MENDAPATKAN KEUNTUNGAN SEBELUM DAN SETELAH ICO, DAN KAMI JUGA MENYEDIAKAN BISNIS YANG SEBENARNYA BERDASARKAN KEINGINAN KAMI DALAM MENJALANKAN TRANSAKSI CRYPTOCURRENCY"
+];
