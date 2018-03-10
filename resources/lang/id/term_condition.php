617b63fa41df86579c24d084aa475ee9be471140
ridhozhr10 <azharridho42@gmail.com>
2018-03-11 02:14:07 +0700

    add lang indo

---
+++ resources/lang/id/term_condition.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	"content" => "<h4>Selamat bergabung dalam dunia bisnis online cryptocurency.</h4>
+	<p>adalah member yang bergabung didalamnya tidak mengikat dan bersedia mengikuti alur yang ada di dalamnya:</p>
+	<ol>
+		<li>User akan sah bila sudah menunjukkan pembayaran, user juga berkewajiban untuk menuliskan data dengan benar (bila dikemudia hari data tidak valid maka kami tidak akan memberikan pertanggungjawaban)</li>
+		<li>Bersama sama bersedia untuk tidak memberikan data kepada pihak manapun agar tidak terjadi penghapusan data secara sepihak.</li>
+		<li>Kami dengan komitmen akan menjaga data base user dengan baik dan dengan tingkat keamanan yang sangat terpercaya.4 Bersedia menerima peraturan yang ada di dalamnya.</li>
+		<li>Bersama sama membangun dan mensuport untuk kemajuan bisnis ini dengan baik.</li>
+		<li>Bersedia menerima resiko kerugian yang di akibatkan oleh keadaan fluktuatif cryptocurency</li>
+		<li>Data atau akun bersedia dibekukan bila data yang diterima adalah tidak valid. Bersama sama mengikuti alur dalam managemen baik deposit dan widrawal yang telah ditentukan oleh perusahaan</li>
+	</ol>"
+];
