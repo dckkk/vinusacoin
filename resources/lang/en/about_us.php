3b357be815b19c768c9fc70806d8cb4df4f50870
ridhozhr10 <azharridho42@gmail.com>
2018-03-10 22:07:05 +0700

    fix biar mirip yee

--- resources/lang/en/about_us.php
+++ resources/lang/en/about_us.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-	"content_1" => "<p style='text-align:justify'>Vinusacoin is a cryptocurrency, Vinusacoin mission and vision to unite the world through cryptorrency trading, which you can trust to gain profit.</p>",
-	"content_2" => "<p style='text-align:justify'>Why you should trust Vinusacoin, because Vinusacoin has its own value unlike the other cryptocurrency, which only determines the value when ICO, since the beginning of Vinusacoin already has good selling points that can be accounted for.</p>",
-	"content_3" => "<p style='text-align:justify'>We must understand that trading in cryptocurency has a high risk of loss, but also has a high profit value as well.</p>",
-	"content_4" => "<p style='text-align:justify'>Due to this fact, we began to develop the cryptocurency trading system technically, because modern cryptocurency can generate more profit with less risk. As we know that in the world of trading capital strength is the main, the more our capital will be the more we get the maximum profit and consistent, Vinusacoin will provide a consistent profit.</p>",
-	"content_5" => "<p style='text-align:justify'>Vinusacoin has an advanced platform with a high level of security. The platform is beginning to spread to Southeast Asia and Europe, enabling anyone to access the Digital Financing, Investment and Trade Service</p>",
-	"content_6" => "<p style='text-align:justify'>By joining Vinusacoin, not only members may access our Digital Financing, Investment, or Commerce Services; in the future as Vinusacoin's journey and development will make it easier for you to betrtransasi online in the fintech world.</p>",
-	"content_7" => "<p style='text-align:center'><strong style='text-align:center'>VINUSACOIN EASILES YOU TO GET THE BENEFITS BEFORE ICO AND AFTER ICO AND WE PROVIDE REAL BUSINESS ISSUES ACCORDING TO OUR ABILITY IN TRYING CRYPTOCURENCY TRADING</strong></p>"
+	"<p style='text-align:justify'>Vinusacoin is a cryptocurrency, Vinusacoin mission and vision to unite the world through cryptorrency trading, which you can trust to gain profit.</p>",
+	"<p style='text-align:justify'>Why you should trust Vinusacoin, because Vinusacoin has its own value unlike the other cryptocurrency, which only determines the value when ICO, since the beginning of Vinusacoin already has good selling points that can be accounted for.</p>",
+	"<p style='text-align:justify'>We must understand that trading in cryptocurency has a high risk of loss, but also has a high profit value as well.</p>",
+	"<p style='text-align:justify'>Due to this fact, we began to develop the cryptocurency trading system technically, because modern cryptocurency can generate more profit with less risk. As we know that in the world of trading capital strength is the main, the more our capital will be the more we get the maximum profit and consistent, Vinusacoin will provide a consistent profit.</p>",
+	"<p style='text-align:justify'>Vinusacoin has an advanced platform with a high level of security. The platform is beginning to spread to Southeast Asia and Europe, enabling anyone to access the Digital Financing, Investment and Trade Service</p>",
+	"<p style='text-align:justify'>By joining Vinusacoin, not only members may access our Digital Financing, Investment, or Commerce Services; in the future as Vinusacoin's journey and development will make it easier for you to betrtransasi online in the fintech world.</p>",
+	"<p style='text-align:center'><strong style='text-align:center'>VINUSACOIN EASILES YOU TO GET THE BENEFITS BEFORE ICO AND AFTER ICO AND WE PROVIDE REAL BUSINESS ISSUES ACCORDING TO OUR ABILITY IN TRYING CRYPTOCURENCY TRADING</strong></p>"
 ];
