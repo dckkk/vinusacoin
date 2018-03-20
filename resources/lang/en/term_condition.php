c1b6383edc80b5e47841f5bf93359f7c4263cfd0
ridhozhr10 <azharridho42@gmail.com>
2018-03-20 21:58:24 +0700

    terms english lang done

--- resources/lang/en/term_condition.php
+++ resources/lang/en/term_condition.php
@@ -1,24 +1,16 @@
 <?php
 
 return [
-	"content" => "<h4>Selamat bergabung dalam dunia bisnis online cryptocurency.</h4>
-	<p>adalah member yang bergabung didalamnya tidak mengikat dan bersedia mengikuti alur yang ada di dalamnya:</p>
-	<ol>
-		<li>User akan sah bila sudah menunjukkan pembayaran, user juga berkewajiban untuk menuliskan data dengan benar (bila dikemudia hari data tidak valid maka kami tidak akan memberikan pertanggungjawaban)</li>
-		<li>Bersama sama bersedia untuk tidak memberikan data kepada pihak manapun agar tidak terjadi penghapusan data secara sepihak.</li>
-		<li>Kami dengan komitmen akan menjaga data base user dengan baik dan dengan tingkat keamanan yang sangat terpercaya.4 Bersedia menerima peraturan yang ada di dalamnya.</li>
-		<li>Bersama sama membangun dan mensuport untuk kemajuan bisnis ini dengan baik.</li>
-		<li>Bersedia menerima resiko kerugian yang di akibatkan oleh keadaan fluktuatif cryptocurency</li>
-		<li>Data atau akun bersedia dibekukan bila data yang diterima adalah tidak valid. Bersama sama mengikuti alur dalam managemen baik deposit dan widrawal yang telah ditentukan oleh perusahaan</li>
-	</ol>",
 	"content" => "<h4>Welcome to world of online cryptocurency business.</h4>
 	<p>member that joined isn't holded and able to follow our business rule inside :</p>
 	<ol>
 		<li>User has been verified once invoice already sended, user must be sended the data carefully (if data isn't valid, we will not provide any responsibility)</li>
-		<li>Toget</li>
-		<li>Kami dengan komitmen akan menjaga data base user dengan baik dan dengan tingkat keamanan yang sangat terpercaya.4 Bersedia menerima peraturan yang ada di dalamnya.</li>
-		<li>Bersama sama membangun dan mensuport untuk kemajuan bisnis ini dengan baik.</li>
-		<li>Bersedia menerima resiko kerugian yang di akibatkan oleh keadaan fluktuatif cryptocurency</li>
-		<li>Data atau akun bersedia dibekukan bila data yang diterima adalah tidak valid. Bersama sama mengikuti alur dalam managemen baik deposit dan widrawal yang telah ditentukan oleh perusahaan</li>
+		<li>Doesn't share any information to another resource so any information won't be removed without any confirmation</li>
+		<li>We will provide best security for any information you provide</li>
+		<li>Agreed with the rule inside</li>
+		<li>Together giving support and contribution in this business</li>
+		<li>Agreed with any loss efected by cryptocurency fluctuate</li>
+		<li>Agreed when information and account getting freezed when information isn't valid.</li>
+		<li>Together following business rule well on deposit and withdrawal that already ruled by our company terms</li>
 	</ol>"
 ];
