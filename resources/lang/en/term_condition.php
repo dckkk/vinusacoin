de3f0cf210d476d1036db62a3053640e44ae0498
ridhozhr10 <azharridho42@gmail.com>
2018-03-20 21:45:16 +0700

    term condition english lang

--- resources/lang/en/term_condition.php
+++ resources/lang/en/term_condition.php
@@ -10,5 +10,15 @@ return [
 		<li>Bersama sama membangun dan mensuport untuk kemajuan bisnis ini dengan baik.</li>
 		<li>Bersedia menerima resiko kerugian yang di akibatkan oleh keadaan fluktuatif cryptocurency</li>
 		<li>Data atau akun bersedia dibekukan bila data yang diterima adalah tidak valid. Bersama sama mengikuti alur dalam managemen baik deposit dan widrawal yang telah ditentukan oleh perusahaan</li>
+	</ol>",
+	"content" => "<h4>Welcome to world of online cryptocurency business.</h4>
+	<p>member that joined isn't holded and able to follow our business rule inside :</p>
+	<ol>
+		<li>User has been verified once invoice already sended, user must be sended the data carefully (if data isn't valid, we will not provide any responsibility)</li>
+		<li>Toget</li>
+		<li>Kami dengan komitmen akan menjaga data base user dengan baik dan dengan tingkat keamanan yang sangat terpercaya.4 Bersedia menerima peraturan yang ada di dalamnya.</li>
+		<li>Bersama sama membangun dan mensuport untuk kemajuan bisnis ini dengan baik.</li>
+		<li>Bersedia menerima resiko kerugian yang di akibatkan oleh keadaan fluktuatif cryptocurency</li>
+		<li>Data atau akun bersedia dibekukan bila data yang diterima adalah tidak valid. Bersama sama mengikuti alur dalam managemen baik deposit dan widrawal yang telah ditentukan oleh perusahaan</li>
 	</ol>"
 ];
