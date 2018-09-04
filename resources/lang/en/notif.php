2d89099645e41af79b09c8aa2d792992fafd7961
Dicky Pratama <dicky.pratama@globalpay.id>
2018-09-04 11:15:40 +0700

    uppercase vinusacoin

--- resources/lang/en/notif.php
+++ resources/lang/en/notif.php
@@ -3,5 +3,5 @@
 return [
 	"content" => "<p style=font-size: 16px;>The 100 seats quota that we open with 8 months contract GPPS 20%/month have been succeed and we officially closed. We provide opportunity to the investor for invest by opening 100 seats quota again with 12 months contract period and GPPS 10%/month.</p>
 		<br>
-		<p style=font-size: 16px;>Accordingly this information we conveyed, we're very thank you to the investor for trust to invest at vinusacoin</p>"
+		<p style=font-size: 16px;>Accordingly this information we conveyed, we're very thank you to the investor for trust to invest at VINUSACOIN</p>"
 ];
